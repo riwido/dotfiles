8418cadd34fed735f42576c50b4b2b11f11810a0
Richard <richard.w.dodson@gmail.com>
2024-04-30 10:28:13 -0700

    Replace existing bash files

--- links.sh
+++ links.sh
@@ -4,9 +4,9 @@ mkdir -p ~/.config/i3
 mkdir -p ~/.vim
 
 ln -s $PWD/i3/config ~/.config/i3/config
-ln -s $PWD/.bash_logout ~/.bash_logout
-ln -s $PWD/.bashrc ~/.bashrc
-ln -s $PWD/.bash_profile ~/.bash_profile
+ln -sf $PWD/.bash_logout ~/.bash_logout
+ln -sf $PWD/.bashrc ~/.bashrc
+ln -sf $PWD/.bash_profile ~/.bash_profile
 ln -s $PWD/.Xresources ~/.Xresources
 ln -s $PWD/.xinitrc ~/.xinitrc
 ln -s $PWD/coc-settings.json ~/.vim/coc-settings.json
