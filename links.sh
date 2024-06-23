7d6fd4d270ba0dfb4f26bb23319867b25938ed4b
Richard <richard.w.dodson@gmail.com>
2024-06-23 14:23:15 +0000

    Add mesen layout

--- links.sh
+++ links.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.config/i3
 mkdir -p ~/.vim
 
-ln -s $PWD/i3/config ~/.config/i3/config
+ln -s $PWD/i3 ~/.config/i3
 ln -sf $PWD/.bash_logout ~/.bash_logout
 ln -sf $PWD/.bashrc ~/.bashrc
 ln -sf $PWD/.bash_profile ~/.bash_profile
@@ -11,4 +11,3 @@ ln -s $PWD/.Xresources ~/.Xresources
 ln -s $PWD/.xinitrc ~/.xinitrc
 ln -s $PWD/coc-settings.json ~/.vim/coc-settings.json
 ln -s $PWD/.vimrc ~/.vimrc
-
