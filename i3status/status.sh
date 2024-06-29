be285007c67b7cd26b06449889783eb5d21ab9fc
Richard <richard.w.dodson@gmail.com>
2024-06-29 14:50:15 -0700

    fix missing pid bug

--- i3status/status.sh
+++ i3status/status.sh
@@ -4,9 +4,10 @@ do
     read line
     WINDOW_ID=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
     WINDOW_CLASS="$(xprop -id $WINDOW_ID | grep -oP 'WM_CLASS.*?, "\K[^"]*')"
+    WINDOW_PID="$(xprop -id $WINDOW_ID | grep -oP '_NET_WM_PID.*= \K\d+')"
     custom="${WINDOW_CLASS}"
     if [[ $WINDOW_CLASS == "firefox" ]]; then
-        profile=$(grep -oP "${pid}=\K.*" $ff_pidfile)
+        profile=$(grep -oP "${WINDOW_PID}=\K.*" $ff_pidfile)
         custom="firefox@${profile}"
     fi
     echo "${custom} | ${line}" || exit 1
