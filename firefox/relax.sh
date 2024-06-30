eab25853bf64844e11e6ccb93a35ee0de2745554
Richard <richard.w.dodson@gmail.com>
2024-06-30 09:45:42 -0700

    Apply relaxed settings to specified profile

---
+++ firefox/relax.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+profiles=$HOME/.mozilla/firefox
+profile=$1
+
+usage (){
+    echo "$0 <profile>"
+}
+if [[ $profile == "-h" ]]; then
+    usage
+    exit 0
+fi
+
+if [[ -z $profile ]]; then
+    usage
+    exit 1
+fi
+
+profile_path=$(find $profiles -maxdepth 1 -type d -name "*.${profile}")
+if [[ -z $profile_path ]]; then
+    echo "$profile doesn't exist"
+    exit 1
+fi
+
+ln -sf $PWD/relaxed.js $profile_path/user.js
---
+++ firefox/relaxed.js
@@ -0,0 +1,7 @@
+// override pyllyukko/user.js settings
+user_pref('dom.serviceWorkers.enabled', true);
+user_pref('dom.event.clipboardevents.enabled', true);
+user_pref('dom.allow_cut_copy', true);
+user_pref('browser.privatebrowsing.autostart', false);
+user_pref('privacy.clearOnShutdown.cookies', false);
+user_pref('network.cookie.lifetimePolicy', 0);
