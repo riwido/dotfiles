62a8dbfd1f57ec4d52e8849811911d3af234fae6
Richard <richard.w.dodson@gmail.com>
2024-10-29 00:58:57 +0000

    cube scrambler

---
+++ scripts/scramble.py
@@ -0,0 +1,24 @@
+import argparse
+import itertools
+import re
+import random
+import sys
+
+moves = list(itertools.chain.from_iterable((m, f"{m}'", f"{m}2") for m in "BRFLUD"))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("count", type=int, nargs="?", default=20)
+    args = parser.parse_args()
+    i = 0
+    while True:
+        i += 1
+        scramble = " ".join(random.choice(moves) for _ in range(args.count))
+        if not re.search(r"(\w)[ 2']+\1", scramble):
+            break
+    print(f"{i}: {scramble}")
+
+
+if __name__ == "__main__":
+    main()
