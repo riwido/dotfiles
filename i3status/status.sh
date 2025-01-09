e2f3b0b4f2f5ecc72d8956877928bc6ad78f7d16
Richard <richard.w.dodson@gmail.com>
2025-01-09 19:41:30 +0000

    show clipboard info in statusbar

--- i3status/status.sh
+++ i3status/status.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+
+clipboard_count () {
+    xcontent=$(xclip -o 2> /dev/null)
+    xlines=$(printf -- "$xcontent" | wc -l)
+    xchars=$(printf -- "$xcontent" | wc -c)
+
+    content=$(xclip -selection clipboard -o 2> /dev/null)
+    lines=$(printf -- "$content" | wc -l)
+    chars=$(printf -- "$content" | wc -c)
+
+    printf "%d %d | %d %d" $xlines $xchars $lines $chars
+    }
+
+
 # config relies on some environment variables
 i3status -c <(envsubst < $HOME/.config/i3status/config) | while :
 do
     read line
-    # custom info not needed, but this is how it's done
-    # output="$(custom) | ${line}";
-    output=$line
+    output="$(clipboard_count) | ${line}";
     echo "${output}" || exit 1
 done
