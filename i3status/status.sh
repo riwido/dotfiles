cc9acffde27c856830c856692c5509072b2a8d53
Richard <richard.w.dodson@gmail.com>
2025-03-31 13:58:22 +0000

    fix printf bug; report chars vs bytes

--- i3status/status.sh
+++ i3status/status.sh
@@ -3,12 +3,12 @@
 
 clipboard_count () {
     xcontent=$(xclip -o 2> /dev/null)
-    xlines=$(printf -- "$xcontent" | wc -l)
-    xchars=$(printf -- "$xcontent" | wc -c)
+    xlines=$(printf "%s" "$xcontent" | wc -l)
+    xchars=$(printf "%s" "$xcontent" | wc -m)
 
     content=$(xclip -selection clipboard -o 2> /dev/null)
-    lines=$(printf -- "$content" | wc -l)
-    chars=$(printf -- "$content" | wc -c)
+    lines=$(printf "%s" "$content" | wc -l)
+    chars=$(printf "%s" "$content" | wc -m)
 
     printf "%d %d | %d %d" $xlines $xchars $lines $chars
     }
@@ -17,7 +17,7 @@ clipboard_count () {
 # config relies on some environment variables
 i3status -c <(envsubst < $HOME/.config/i3status/config) | while :
 do
-    read line
+    read -r line
     output="$(clipboard_count) | ${line}";
     echo "${output}" || exit 1
 done
